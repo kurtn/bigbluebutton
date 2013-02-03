4404624fcfec8c6e82372362e671bd5e0d35887a
Richard Alam <ritzalam@gmail.com>
2013-02-03 15:21:22 +0000

    - finally logging...typo error on closing xml tag...

--- bigbluebutton-apps/src/main/scala/org/bigbluebutton/live/BigBlueButton.scala
+++ bigbluebutton-apps/src/main/scala/org/bigbluebutton/live/BigBlueButton.scala
@@ -3,17 +3,18 @@ package org.bigbluebutton.live
 import akka.actor.ActorSystem
 import com.typesafe.config.ConfigFactory
 import akka.event.Logging
-//import akka.event.LogSource
 import org.slf4j.LoggerFactory
+import akka.actor.Actor
+import akka.actor.Props
 
 object BigBlueButton {
-//  val log = LoggerFactory.getLogger("bigBlueButton")
+//  val log = LoggerFactory.getLogger("BigBlueButton")
   
   println("************ Instantiating BigBlueButton System ***************")
   val config = ConfigFactory.load()
 //  val system = ActorSystem("BigBlueButton")
   val system = ActorSystem("BigBlueButton", config.getConfig("myapp1").withFallback(config))
-//  val log = Logging(system.eventStream)
+//  val log = Logging(system, this)
 
   val log = Logging(system.eventStream, "bigbluebutton")
 //  val log = Logging(system, classOf[BigBlueButton])
@@ -23,5 +24,26 @@ object BigBlueButton {
 	def hello = {
 	  println("************* Hello world! [{}] ****************", config.getNumber("myapp1.my.own.setting"))
 	  log.debug("********* my.own.setting = {} ****************", config.getNumber("myapp1.my.own.setting"))
+	  
+	  val ma = system.actorOf(Props[MyActor], "myActor")
+	  ma ! "test"
+	  ma ! "unknown message"
 	}
+}
+
+class MyActor extends Actor {
+  val log = Logging(context.system, this)
+  override def preStart() = {
+    log.debug("Starting")
+  }
+  
+  override def preRestart(reason: Throwable, message: Option[Any]) {
+    log.error(reason, "Restarting due to [{}] when processing [{}]",
+      reason.getMessage, message.getOrElse(""))
+  }
+  
+  def receive = {
+    case "test" => log.info("Received test")
+    case x      => log.warning("Received unknown message: {}", x)
+  }
 }
